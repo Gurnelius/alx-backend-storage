12a534fbae296bc7be9049fd4a309c75ea6004bd
Gurnelius Terer <terergurnelius@gmail.com>
2024-07-17 16:55:44 +0300

    Initial commit

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,6 +1,12 @@
 def top_students(mongo_collection):
     """Return all students sorted by average score"""
     return list(mongo_collection.aggregate([
-        { "$addFields": { "averageScore": { "$avg": "$topics.score" } } },
-        { "$sort": { "averageScore": -1 } }
+        {
+            "$addFields": {
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
     ]))
